9ee879bcba4ebbd33564f6931d1cb2d812fac603
pagiron <pagiron@scl.edu.gt>
2024-06-20 18:31:26 -0600

    Actualización de pantalla

--- components/evolucion6/evolucion6.js
+++ components/evolucion6/evolucion6.js
@@ -1,13 +1,12 @@
-
 let DOM_evolucion6 = document.querySelector("#evolucion6");
 DOM_evolucion6.innerHTML = `
 <body>
-    <header class="Pablo1">
+    <header class="header">
         <h1>Evolución de los Televisores</h1>
     </header>
     <div class="timeline">
-        <div class="container left">
-            <div class="content">
+        <div class="container manzana">
+            <div class="lapiz">
                 <h2>(1920s-1930s)</h2>
                 <h1>Televisores Mecanicos</h1>
                 <p>Los primeros televisores que se pueden considerar comerciales fueron de tipo mecánico y se basaban en un disco giratorio, el disco de Nipkow (patentado por el ingeniero alemán Paul Nipkow en 1884), que contenía una serie de agujeros dispuestos en espiral y que permitían realizar una exploración «línea por línea»</p>
@@ -17,10 +16,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        
-        <div class="container right">
-            <div class="content">
+        <div class="container platano">
+            <div class="cuaderno">
                 <h2>(1940s-2000s)</h2>
                 <h1>Televisores de Tubo de Rayos Catódicos</h1>
                <p>Exteriormente se colocan unas bobinas que controlan un campo magnético que desplaza el haz de electrones por toda la pantalla, iluminándola en todos sus puntos. Controlando adecuadamente la intensidad del haz en cada momento, se genera una imagen en la pantalla.</p>
@@ -30,11 +27,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        
-        
-        <div class="container left">
-            <div class="content">
+        <div class="container manzana">
+            <div class="lapiz">
                 <h2>(1970s-2000s)</h2>
                 <h1>Televisores de Proyección</h1>
                 <p>La TV a retro-proyección o TV de proyección trasera1​ (RPTV) es un tipo de TV en pantalla grande en la que el aparato receptor a partir de una señal de TV o de vídeo mediante una fuente de luz muy brillante, proyecta la correspondiente imagen (en movimiento) en la parte trasera de una pantalla de proyección translúcida a través de un sistema de lentes y espejos, permitiendo así mostrar imágenes en pantallas de gran tamaño..</p>
@@ -44,11 +38,9 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        
-        <div class="container right">
-            <div class="content">
-                <h2>(1990s-2010s)</h1>
+        <div class="container platano">
+            <div class="cuaderno">
+                <h2>(1990s-2010s)</h2>
                 <h1>Televisores Plasma</h1>
                 <p>Innovación: Pantallas que se pueden doblar o enrollar, y tecnología MicroLED que promete mejor calidad de imagen y durabilidad que OLED. Futuro: Potencial para aplicaciones más allá de los televisores tradicionales, como en dispositivos portátiles y otros gadgets.</p>
                 <div class="foto4">
@@ -57,10 +49,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        
-        <div class="container left">
-            <div class="content">
+        <div class="container manzana">
+            <div class="lapiz">
                 <h2>(Años 2000 en adelante)</h2>
                 <h1>Televisores LCD</h1>
                 <p>Una pantalla de cristal líquido o LCD (sigla del inglés liquid-crystal display) es una pantalla plana y delgada formada por un número de píxeles en color o monocromos colocados delante de una fuente de luz o reflectora.</p>
@@ -70,10 +60,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        
-        <div class="container right">
-            <div class="content">
+        <div class="container platano">
+            <div class="cuaderno">
                 <h2>(Años 2010 en adelante)</h2>
                 <h1>Televisores LED</h1>
                 <p>Una pantalla LED, o pantalla de diodos emisores de luz, es una pantalla plana que utiliza diodos emisores de luz como pantalla de vídeo. Una pantalla LED puede ser una pantalla pequeña o parte de una pantalla más grande. Los diodos LED se utilizan para formar una pantalla LED.</p>
@@ -83,9 +71,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        <div class="container left">
-            <div class="content">
+        <div class="container manzana">
+            <div class="lapiz">
                 <h2>(Años 2010 en adelante)</h2>
                 <h1>Televisores OLED</h1>
                 <p>Un televisor OLED es un tipo de TV que utiliza tecnología de pantalla orgánica para producir luz, permitiendo que cada píxel se ilumine de forma individual y generando negros más profundos y colores más precisos. Los televisores OLED son conocidos por su delgadez, flexibilidad y eficiencia energética.</p>
@@ -96,9 +83,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        <div class="container right">
-            <div class="content">
+        <div class="container platano">
+            <div class="cuaderno">
                 <h2>(Años 2010 en adelante)</h2>
                 <h1>Televisores 4K y 8K</h1>
                 <p>Para ser más exactos, un televisor 4k suele tener una resolución de 3840 x 2160 (3840 filas de píxeles horizontales y 2160 verticales), mientras que un 8K tiene una resolución de 7680 x 4320. En otras palabras, un televisor 8K es capaz de reproducir imágenes, en teoría, con cuatro veces más detalle que un 4k.27</p>
@@ -109,9 +95,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        <div class="container left">
-            <div class="content">
+        <div class="container manzana">
+            <div class="lapiz">
                 <h2>(2015)</h2>
                 <h1>HDR</h1>
                 <p>Los televisores con tecnología HDR (High Dynamic Range) son ideales para una amplia variedad de programas y contenidos. En particular, son excelentes para ver películas y series, ya que permiten una experiencia visual más inmersiva y detallada gracias a su mayor capacidad para reproducir colores y contraste.</p>
@@ -123,9 +108,8 @@ DOM_evolucion6.innerHTML = `
             </div>
         </div>
         
-        
-        <div class="container right">
-            <div class="content">
+        <div class="container platano">
+            <div class="cuaderno">
                 <h2>(Años 2020 en adelante)</h2>
                 <h1>Pantallas Flexibles y MicroLED</h1>
                 <p>Televisores con pantallas flexibles son dispositivos de visualización que utilizan tecnologías avanzadas, como OLED (diodos orgánicos emisores de luz) o MicroLED, para permitir que la pantalla se doble, curve o incluso se enrolle. Estas pantallas están hechas con materiales flexibles que permiten que el dispositivo tenga una forma no plana sin comprometer la calidad de la imagen. </p>
@@ -135,11 +119,11 @@ DOM_evolucion6.innerHTML = `
             </div>
            
         </div>
-        <footer class="girón1">
+        
+        <footer class="footer">
             <h1>©pagiron@scl.edu.gt</h1>
            </footer>
     </div>
     <script src="scripts.js"></script>
 </body>
 `;
-
